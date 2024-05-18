d92585a5f382ec3acd21b52737d034c1e01747bc
khan2626 <israelkhan217@gmail.com>
2024-05-18 15:49:43 +0100

    Connected to mongodb

---
+++ utils/db.js
@@ -0,0 +1,47 @@
+import { MongoClient } from 'mongodb';
+
+
+const DB_HOST = process.env.DB_HOST || 'localhost';
+const DB_PORT = process.env.DB_PORT || 27017;
+const DB_DATABASE = process.env.DB_DATABASE || 'files_manager';
+const url = `mongodb://${DB_HOST}:${DB_PORT}/${DB_DATABASE}`;
+
+
+class DBClient {
+ constructor() {
+   MongoClient.connect(url, { useUnifiedTopology: true }, (err, client) => {
+     if (err) {
+       console.log(err);
+       this.connected = false;
+     } else {
+       this.client = client.db(DB_DATABASE);
+       this.users = this.client.collection('users');
+       this.projects = this.client.collection('projects');
+       this.artisans = this.client.collection('artisans');
+       this.reviews = this.client.collection('reviews');
+       this.connected = true;
+     }
+   });
+ }
+
+
+ isAlive() {
+   return Boolean(this.connected);
+ }
+
+
+ async nbUsers() {
+   const count = await this.users.countDocuments();
+   return count;
+ }
+
+
+ async nbProjects() {
+   const count = await this.projects.countDocuments();
+   return count;
+ }
+}
+
+
+const dbClient = new DBClient();
+export default dbClient;
