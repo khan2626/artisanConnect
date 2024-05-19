d7a964db7a6dcb25326de5844c06522e0e517f62
khan2626 <israelkhan217@gmail.com>
2024-05-19 14:58:00 +0100

    created schemas with mongoose

--- utils/db.js
+++ utils/db.js
@@ -1,5 +1,5 @@
-import { MongoClient } from 'mongodb';
-
+//import { MongoClient } from 'mongodb';
+const { MongoClient } = require('mongodb');
 
 const DB_HOST = process.env.DB_HOST || 'localhost';
 const DB_PORT = process.env.DB_PORT || 27017;
