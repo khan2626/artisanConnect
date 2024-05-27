c0c064a440ed9fba0c0f99e5990808eaf677cc68
khan2626 <israelkhan217@gmail.com>
2024-05-27 01:21:26 +0100

    implement refresh token route

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -5,8 +5,8 @@ const RefreshToken = require('../models/refreshToken');
 const bcrypt = require('bcryptjs');
 const router = express.Router();
 
-const accessSecret = 'my_secret'
-const refreshSeret = 'my_refresh_secret'
+const accessSecret = 'my_secret';
+const refreshSeret = 'my_refresh_secret';
 
 router.post('/login', async (req, res) => {
     try {
@@ -21,10 +21,10 @@ router.post('/login', async (req, res) => {
         if (!isMatch) {
             return res.status(401).json({ message: 'Incorrect password' });
         }
-        const accessToken = jwt.sign({ userId: user._id }, refreshSeret, { expiresIn: '15m'} );
+        const accessToken = jwt.sign({ userId: user._id }, accessSecret, { expiresIn: '15m'} );
         res.status(200).json(accessToken)
 
-        const refreshToken = jwt.sign({ userId: user._id }, accessSecret, { expiresIn: '14d'} );
+        const refreshToken = jwt.sign({ userId: user._id }, refreshSeret, { expiresIn: '14d'} );
         //save the refresh token to the database
         await new RefreshToken({ token: refreshToken, userId: user._id }).save();
 
@@ -33,6 +33,29 @@ router.post('/login', async (req, res) => {
 
     } catch (error) {
         res.status(500).json({ message: error.message})
+    }
+});
+
+
+router.post('/refresh-token', async (req, res) => {
+    const refreshToken = req.cookies.refreshToken;
+
+    if (!refreshToken) {
+        res.status(401).json(message: 'No refresh token provided');
+    }
+
+    try {
+        const decoded = jwt.sign(refreshToken, refreshSeret);
+        const storedToken = await RefreshToken.find({ token: refreshToken, userId: decoded.userId });
+
+        if (!storedToken) {
+            res.status(401).json({ message: 'invalid refresh token' });
+        }
+
+        const accessToken = jwt.sign({ userId: decoded.userId }, accessSecret, { expiresIn: '15m'});
+        res.status(200).json(accessToken);
+    } catch(error) {
+        res.status(401).json(error.message);
     }
 });
 
